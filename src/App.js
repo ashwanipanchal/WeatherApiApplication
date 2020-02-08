f70b14ce14349d6b609a76cf51da06d051f8a793
Ashwani Panchal <ashwani.panchal@hotmail.com>
2020-02-08 19:56:54 +0530

    weather api acess and work on UI

--- src/App.js
+++ src/App.js
@@ -1,26 +1,69 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'weather-icons/css/weather-icons.min.css'
+import Weather from './components/Weather';
+// import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const API_KEY="03b65e5b66ee6cb40a435eafc29b317a";
+
+class App extends React.Component {
+
+  constructor(){
+    super();
+    this.state={
+      city : undefined,
+      country:undefined,
+      temp:undefined,
+      celsius:undefined,
+      minTemp:undefined,
+      maxTemp:undefined,
+      description:undefined,
+    }
+    this.getWeather();
+
+  }
+
+  getCelsius(temp){
+    let cell = Math.floor(temp - 273.15) ;
+    return cell
+  }
+
+  getWeather=async ()=>{
+    const api_call =await fetch(`http://api.openweathermap.org/data/2.5/weather?q=Delhi,in&apikey=${API_KEY}`);
+    const response =await api_call.json();
+    console.log(response);
+
+    this.setState({
+      city : response.name,
+      country: response.sys.country,
+      temp : response.main.temp,
+      celsius : this.getCelsius(response.main.temp),
+      minTemp : this.getCelsius(response.main.temp_min),
+      maxTemp : this.getCelsius(response.main.temp_max),
+      description : response.weather[0].main,
+
+    })
+  }
+
+  render(){
+    return (
+      <>
+        <Weather
+          city={this.state.city}
+          country={this.state.country}
+          celsius={this.state.celsius}
+          minTemp={this.state.minTemp}
+          maxTemp={this.state.maxTemp}
+          description={this.state.description}
+
+        />
+      </>
+    );
+  }  
 }
+
+
 
 export default App;
